d8814c8b75b318d1eda3201c55e0d17fb8571a05
Ajay Sankar <ajay0196@gmail.com>
2020-11-15 00:10:56 -0800

    Added tab navigator..

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
 import {View, Text, StyleSheet} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import UserFeed from './UserFeed';
+import NicNames from './Nicknames';
+import HashTags from './HashTagsTab';
+import NewPostForm from './NewPostForm';
+
+const Tab = createBottomTabNavigator();
 
 const DashBoard = () => {
   return (
-    <View>
-      <Text> DashBoard </Text>
-    </View>
+    <Tab.Navigator
+      tabBarOptions={{
+        labelStyle: {
+          fontSize: 20,
+          margin: 0,
+          padding: 0,
+        },
+      }}>
+      <Tab.Screen name="Home" component={UserFeed} />
+      <Tab.Screen name="Friends" component={NicNames} />
+      <Tab.Screen name="HashTags" component={HashTags} />
+      <Tab.Screen name="New" component={NewPostForm} />
+    </Tab.Navigator>
   );
 };
 
---
+++ screens/Nicknames.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View, Text, StyleSheet} from 'react-native';
+
+const NickNames = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text> Nicknames </Text>
+    </View>
+  );
+};
+
+export default NickNames;
