134fb96eb16f49aa88d625047a4bd3722ad7c924
Ajay Sankar <ajay0196@gmail.com>
2020-11-14 18:22:12 -0800

    Request states enum..

---
+++ utils/requestStateEnums.js
@@ -0,0 +1,8 @@
+const RequestStates = {
+  NotRequested: 'NotRequested',
+  RequestSuccessful: 'RequestSuccessful',
+  RequestInProcess: 'RequestInProcess',
+  RequestFailed: 'RequestFailed',
+};
+
+export default RequestStates;
