c04dfc1aea2d0e78d6b2a1fce6e273d3784a79a5
Ajay Sankar <ajay0196@gmail.com>
2020-11-16 14:34:44 -0800

    new comment text input..

--- screens/Comments.js
+++ screens/Comments.js
@@ -1,30 +1,42 @@
 import React from 'react';
 
-// There is some require
+// There is some require circular dependency warning with react-native-paper. So importing components seperately
 import ListItem from 'react-native-paper/lib/commonjs/components/List/ListItem';
 import Divider from 'react-native-paper/lib/commonjs/components/Divider';
 import ListAccordion from 'react-native-paper/lib/commonjs/components/List/ListAccordion';
+import TextInput from '../node_modules/react-native-paper/lib/commonjs/components/TextInput/TextInput';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import {View, Text, StyleSheet, TextInput} from 'react-native';
+import {StyleSheet} from 'react-native';
 
 const Comments = () => {
   const comments = ['Comments 1', 'Comments 2', 'Comments 3'];
   return (
     <>
       <TextInput
-        placeholder="Add a comment"
-        style={{
-          paddingHorizontal: 15,
-          marginTop: 50,
-          borderBottomWidth: 0.5,
-          marginHorizontal: 10,
-        }}
+        mode="outlined"
+        label="Add a comment"
+        right={
+          <TextInput.Icon name="comment-plus-outline" onPress={() => {}} />
+        }
       />
-      <ListAccordion title="Load for comments">
-        <ListItem title="First item" />
-        <Divider />
-        <ListItem title="Second item" />
+      <ListAccordion
+        title="Load for comments"
+        titleStyle={styles.accordianTitleStyle}
+        left={() => <MaterialCommunityIcons name="comment" size={15} />}>
+        {comments.map((comment, index) => (
+          <SingleComment key={index} comment={comment} />
+        ))}
       </ListAccordion>
+    </>
+  );
+};
+
+const SingleComment = ({comment}) => {
+  return (
+    <>
+      <ListItem title={comment} />
+      <Divider />
     </>
   );
 };
@@ -32,6 +44,9 @@ const Comments = () => {
 const styles = StyleSheet.create({
   container: {
     alignSelf: 'center',
+  },
+  accordianTitleStyle: {
+    color: '#3b5998',
   },
 });
 
